16713d5907986e3ff802d043e092ce4880c46967
teowa <104055472+teowa@users.noreply.github.com>
2024-06-19 08:24:18 +0000

    wip

--- internal/services/azurestackhci/stack_hci_logical_network_resource.go
+++ internal/services/azurestackhci/stack_hci_logical_network_resource.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/location"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/tags"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2024-01-01/logicalnetworks"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/extendedlocation/2021-08-15/customlocations"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
@@ -51,7 +52,7 @@ type StackHCISubnetModel struct {
 	IpAllocationMethod string                `tfschema:"ip_allocation_method"`
 	IpPool             []StackHCIIPPoolModel `tfschema:"ip_pool"`
 	Route              []StackHCIRouteModel  `tfschema:"route"`
-	Vlan               string                `tfschema:"vlan"`
+	Vlan               int64                 `tfschema:"vlan"`
 }
 
 type StackHCIIPPoolModel struct {
@@ -104,6 +105,7 @@ func (StackHCILogicalNetworkResource) Arguments() map[string]*pluginsdk.Schema {
 		"subnet": {
 			Type:     pluginsdk.TypeList,
 			Required: true,
+			ForceNew: true,
 			Elem: &pluginsdk.Resource{
 				Schema: map[string]*pluginsdk.Schema{
 					"ip_allocation_method": {
@@ -164,9 +166,9 @@ func (StackHCILogicalNetworkResource) Arguments() map[string]*pluginsdk.Schema {
 					},
 
 					"vlan": {
-						Type:         pluginsdk.TypeString,
+						Type:         pluginsdk.TypeInt,
 						Optional:     true,
-						ValidateFunc: validation.StringIsNotEmpty,
+						ValidateFunc: validation.IntPositive,
 					},
 				},
 			},
@@ -205,23 +207,30 @@ func (r StackHCILogicalNetworkResource) Create() sdk.ResourceFunc {
 			payload := logicalnetworks.LogicalNetworks{
 				Name:     pointer.To(config.Name),
 				Location: location.Normalize(config.Location),
+				Tags:     tags.Expand(config.Tags),
 				ExtendedLocation: &logicalnetworks.ExtendedLocation{
 					Name: pointer.To(config.CustomLocationId),
 					Type: pointer.To(logicalnetworks.ExtendedLocationTypesCustomLocation),
 				},
 				Properties: &logicalnetworks.LogicalNetworkProperties{
+					VMSwitchName: pointer.To(config.VmSwitchName),
+					Subnets:      ExpandStackHCILogicalNetworkSubnet(config.Subnet),
 					DhcpOptions: &logicalnetworks.LogicalNetworkPropertiesDhcpOptions{
 						DnsServers: pointer.To(config.DNSServers),
 					},
-					Subnets: ExpandStackHCILogicalNetworkSubnet(config.Subnet),
 				},
 			}
 
-			if err := client.CreateOrUpdateThenPoll(ctx, id, payload); err != nil {
-				return fmt.Errorf("creating %s: %+v", id, err)
+			future, err := client.CreateOrUpdate(ctx, id, payload)
+			if err != nil {
+				return fmt.Errorf("performing create %s: %+v", id, err)
 			}
 
 			metadata.SetID(id)
+
+			if err := future.Poller.PollUntilDone(ctx); err != nil {
+				return fmt.Errorf("polling after create %s: %+v", id, err)
+			}
 
 			return nil
 		},
@@ -232,7 +241,51 @@ func (r StackHCILogicalNetworkResource) Read() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 5 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
-			return nil
+			client := metadata.Client.AzureStackHCI.LogicalNetworks
+
+			id, err := logicalnetworks.ParseLogicalNetworkID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			resp, err := client.Get(ctx, *id)
+			if err != nil {
+				if response.WasNotFound(resp.HttpResponse) {
+					return metadata.MarkAsGone(id)
+				}
+
+				return fmt.Errorf("retrieving %s: %+v", id, err)
+			}
+
+			schema := StackHCILogicalNetworkResourceModel{
+				Name:              id.LogicalNetworkName,
+				ResourceGroupName: id.ResourceGroupName,
+			}
+
+			if model := resp.Model; model != nil {
+				schema.Location = location.Normalize(model.Location)
+				schema.Tags = tags.Flatten(model.Tags)
+
+				if model.ExtendedLocation != nil && model.ExtendedLocation.Name != nil {
+					customLocationId, err := customlocations.ParseCustomLocationIDInsensitively(*model.ExtendedLocation.Name)
+					if err != nil {
+						return err
+					}
+
+					schema.CustomLocationId = customLocationId.ID()
+				}
+
+				if props := model.Properties; props != nil {
+					schema.Subnet = FlattenStackHCILogicalNetworkSubnet(props.Subnets)
+					schema.VmSwitchName = pointer.From(props.VMSwitchName)
+
+					if props.DhcpOptions != nil {
+						schema.DNSServers = pointer.From(props.DhcpOptions.DnsServers)
+					}
+				}
+			}
+
+			return metadata.Encode(&schema)
 		},
 	}
 }
@@ -241,6 +294,44 @@ func (r StackHCILogicalNetworkResource) Update() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.AzureStackHCI.LogicalNetworks
+
+			id, err := logicalnetworks.ParseLogicalNetworkID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			var model StackHCILogicalNetworkResourceModel
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			resp, err := client.Get(ctx, *id)
+			if err != nil {
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			parameters := resp.Model
+			if parameters == nil {
+				return fmt.Errorf("retrieving %s: model was nil", *id)
+			}
+
+			if metadata.ResourceData.HasChange("tags") {
+				parameters.Tags = tags.Expand(model.Tags)
+			}
+
+			if metadata.ResourceData.HasChanges("dns_servers") {
+				if parameters.Properties == nil {
+					parameters.Properties = &logicalnetworks.LogicalNetworkProperties{}
+				}
+				parameters.Properties.DhcpOptions = &logicalnetworks.LogicalNetworkPropertiesDhcpOptions{
+					DnsServers: pointer.To(model.DNSServers),
+				}
+			}
+
+			if err := client.CreateOrUpdateThenPoll(ctx, *id, *parameters); err != nil {
+				return fmt.Errorf("updating %s: %+v", id, err)
+			}
 			return nil
 		},
 	}
@@ -250,6 +341,17 @@ func (r StackHCILogicalNetworkResource) Delete() sdk.ResourceFunc {
 	return sdk.ResourceFunc{
 		Timeout: 30 * time.Minute,
 		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.AzureStackHCI.LogicalNetworks
+
+			id, err := logicalnetworks.ParseLogicalNetworkID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			if err := client.DeleteThenPoll(ctx, *id); err != nil {
+				return fmt.Errorf("deleting %s: %+v", id, err)
+			}
+
 			return nil
 		},
 	}
@@ -267,11 +369,34 @@ func ExpandStackHCILogicalNetworkSubnet(input []StackHCISubnetModel) *[]logicaln
 				AddressPrefix:      pointer.To(v.AddressPrefix),
 				IPAllocationMethod: pointer.To(logicalnetworks.IPAllocationMethodEnum(v.IpAllocationMethod)),
 				IPPools:            ExpandStackHCILogicalNetworkIPPool(v.IpPool),
+				RouteTable:         ExpandStackHCILogicalNetworkRouteTable(v.Route),
+				Vlan:               pointer.To(v.Vlan),
 			},
 		})
 	}
 
 	return &results
+}
+
+func FlattenStackHCILogicalNetworkSubnet(input *[]logicalnetworks.Subnet) []StackHCISubnetModel {
+	if input == nil {
+		return make([]StackHCISubnetModel, 0)
+	}
+
+	results := make([]StackHCISubnetModel, 0)
+	for _, v := range *input {
+		if v.Properties != nil {
+			results = append(results, StackHCISubnetModel{
+				AddressPrefix:      pointer.From(v.Properties.AddressPrefix),
+				IpAllocationMethod: string(pointer.From(v.Properties.IPAllocationMethod)),
+				IpPool:             FlattenStackHCILogicalNetworkIPPool(v.Properties.IPPools),
+				Route:              FlattenStackHCILogicalNetworkRouteTable(v.Properties.RouteTable),
+				Vlan:               pointer.From(v.Properties.Vlan),
+			})
+		}
+	}
+
+	return results
 }
 
 func ExpandStackHCILogicalNetworkIPPool(input []StackHCIIPPoolModel) *[]logicalnetworks.IPPool {
@@ -288,4 +413,63 @@ func ExpandStackHCILogicalNetworkIPPool(input []StackHCIIPPoolModel) *[]logicaln
 	}
 
 	return &results
+}
+
+func FlattenStackHCILogicalNetworkIPPool(input *[]logicalnetworks.IPPool) []StackHCIIPPoolModel {
+	if input == nil {
+		return make([]StackHCIIPPoolModel, 0)
+	}
+
+	results := make([]StackHCIIPPoolModel, 0)
+	for _, v := range *input {
+		results = append(results, StackHCIIPPoolModel{
+			Start: pointer.From(v.Start),
+			End:   pointer.From(v.End),
+		})
+	}
+
+	return results
+}
+
+func ExpandStackHCILogicalNetworkRouteTable(input []StackHCIRouteModel) *logicalnetworks.RouteTable {
+	if len(input) == 0 {
+		return nil
+	}
+
+	routes := make([]logicalnetworks.Route, 0)
+	for _, v := range input {
+		routes = append(routes, logicalnetworks.Route{
+			Name: pointer.To(v.Name),
+			Properties: &logicalnetworks.RoutePropertiesFormat{
+				AddressPrefix:    pointer.To(v.AddressPrefix),
+				NextHopIPAddress: pointer.To(v.NextHopIpAddress),
+			},
+		})
+	}
+
+	return &logicalnetworks.RouteTable{
+		Properties: &logicalnetworks.RouteTablePropertiesFormat{
+			Routes: pointer.To(routes),
+		},
+	}
+}
+
+func FlattenStackHCILogicalNetworkRouteTable(input *logicalnetworks.RouteTable) []StackHCIRouteModel {
+	if input == nil || input.Properties == nil || input.Properties.Routes == nil {
+		return make([]StackHCIRouteModel, 0)
+	}
+
+	results := make([]StackHCIRouteModel, 0)
+	for _, v := range *input.Properties.Routes {
+		route := StackHCIRouteModel{
+			Name: pointer.From(v.Name),
+		}
+		if v.Properties != nil {
+			route.AddressPrefix = pointer.From(v.Properties.AddressPrefix)
+			route.NextHopIpAddress = pointer.From(v.Properties.NextHopIPAddress)
+		}
+		results = append(results, route)
+	}
+
+	return results
 }
--- internal/services/azurestackhci/stack_hci_logical_network_resource_test.go
+++ internal/services/azurestackhci/stack_hci_logical_network_resource_test.go
@@ -1,1 +1,226 @@
 package azurestackhci_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/azurestackhci/2024-01-01/logicalnetworks"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+)
+
+type StackHCILogicalNetworkResource struct{}
+
+func TestAccStackHCILogicalNetwork_basic(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_stack_hci_logical_network", "test")
+	r := StackHCILogicalNetworkResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccStackHCILogicalNetwork_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_stack_hci_logical_network", "test")
+	r := StackHCILogicalNetworkResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
+func TestAccStackHCILogicalNetwork_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_stack_hci_logical_network", "test")
+	r := StackHCILogicalNetworkResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func (r StackHCILogicalNetworkResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	clusterClient := client.AzureStackHCI.LogicalNetworks
+	id, err := logicalnetworks.ParseLogicalNetworkID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := clusterClient.Get(ctx, *id)
+	if err != nil {
+		if response.WasNotFound(resp.HttpResponse) {
+			return utils.Bool(false), nil
+		}
+
+		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
+	}
+
+	return utils.Bool(resp.Model != nil), nil
+}
+
+func (r StackHCILogicalNetworkResource) basic(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%s
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_stack_hci_logical_network" "test" {
+  
+}
+`, template)
+}
+
+func (r StackHCILogicalNetworkResource) requiresImport(data acceptance.TestData) string {
+	config := r.basic(data)
+
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_stack_hci_logical_network" "import" {
+  stack_hci_cluster_id = azurerm_stack_hci_cluster.test.id
+  arc_resource_ids     = azurerm_stack_hci_logical_network.test.arc_resource_ids
+  version              = azurerm_stack_hci_logical_network.test.version
+
+  scale_unit {
+    adou_path                     = azurerm_stack_hci_logical_network.test.scale_unit.0.adou_path
+    domain_fqdn                   = azurerm_stack_hci_logical_network.test.scale_unit.0.domain_fqdn
+    secrets_location              = azurerm_stack_hci_logical_network.test.scale_unit.0.secrets_location
+    naming_prefix                 = azurerm_stack_hci_logical_network.test.scale_unit.0.naming_prefix
+    streaming_data_client_enabled = azurerm_stack_hci_logical_network.test.scale_unit.0.streaming_data_client_enabled
+    eu_location_enabled           = azurerm_stack_hci_logical_network.test.scale_unit.0.eu_location_enabled
+    episodic_data_upload_enabled  = azurerm_stack_hci_logical_network.test.scale_unit.0.episodic_data_upload_enabled
+
+    bitlocker_boot_volume_enabled   = azurerm_stack_hci_logical_network.test.scale_unit.0.bitlocker_boot_volume_enabled
+    bitlocker_data_volume_enabled   = azurerm_stack_hci_logical_network.test.scale_unit.0.bitlocker_data_volume_enabled
+    credential_guard_enabled        = azurerm_stack_hci_logical_network.test.scale_unit.0.credential_guard_enabled
+    drift_control_enabled           = azurerm_stack_hci_logical_network.test.scale_unit.0.drift_control_enabled
+    drtm_protection_enabled         = azurerm_stack_hci_logical_network.test.scale_unit.0.drtm_protection_enabled
+    hvci_protection_enabled         = azurerm_stack_hci_logical_network.test.scale_unit.0.hvci_protection_enabled
+    side_channel_mitigation_enabled = azurerm_stack_hci_logical_network.test.scale_unit.0.side_channel_mitigation_enabled
+    smb_cluster_encryption_enabled  = azurerm_stack_hci_logical_network.test.scale_unit.0.smb_cluster_encryption_enabled
+    smb_signing_enabled             = azurerm_stack_hci_logical_network.test.scale_unit.0.smb_signing_enabled
+    wdac_enabled                    = azurerm_stack_hci_logical_network.test.scale_unit.0.wdac_enabled
+
+
+    cluster {
+      azure_service_endpoint = azurerm_stack_hci_logical_network.test.scale_unit.0.cluster.0.azure_service_endpoint
+      cloud_account_name     = azurerm_stack_hci_logical_network.test.scale_unit.0.cluster.0.cloud_account_name
+      name                   = azurerm_stack_hci_logical_network.test.scale_unit.0.cluster.0.name
+      witness_type           = azurerm_stack_hci_logical_network.test.scale_unit.0.cluster.0.witness_type
+      witness_path           = azurerm_stack_hci_logical_network.test.scale_unit.0.cluster.0.witness_path
+    }
+
+    host_network {
+      intent {
+        name         = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.0.name
+        adapter      = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.0.adapter
+        traffic_type = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.0.traffic_type
+      }
+
+      intent {
+        name         = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.1.name
+        adapter      = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.1.adapter
+        traffic_type = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.intent.1.traffic_type
+      }
+
+      storage_network {
+        name                 = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.0.name
+        network_adapter_name = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.0.network_adapter_name
+        vlan_id              = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.0.vlan_id
+      }
+
+      storage_network {
+        name                 = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.1.name
+        network_adapter_name = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.1.network_adapter_name
+        vlan_id              = azurerm_stack_hci_logical_network.test.scale_unit.0.host_network.0.storage_network.1.vlan_id
+      }
+    }
+
+    infrastructure_network {
+      gateway     = azurerm_stack_hci_logical_network.test.scale_unit.0.infrastructure_network.0.gateway
+      subnet_mask = azurerm_stack_hci_logical_network.test.scale_unit.0.infrastructure_network.0.subnet_mask
+      dns_server  = azurerm_stack_hci_logical_network.test.scale_unit.0.infrastructure_network.0.dns_server
+      ip_pool {
+        ending_address   = azurerm_stack_hci_logical_network.test.scale_unit.0.infrastructure_network.0.ip_pool.0.ending_address
+        starting_address = azurerm_stack_hci_logical_network.test.scale_unit.0.infrastructure_network.0.ip_pool.0.starting_address
+      }
+    }
+
+    optional_service {
+      custom_location = azurerm_stack_hci_logical_network.test.scale_unit.0.optional_service.0.custom_location
+    }
+
+    physical_node {
+      ipv4_address = azurerm_stack_hci_logical_network.test.scale_unit.0.physical_node.0.ipv4_address
+      name         = azurerm_stack_hci_logical_network.test.scale_unit.0.physical_node.0.name
+    }
+
+    physical_node {
+      ipv4_address = azurerm_stack_hci_logical_network.test.scale_unit.0.physical_node.1.ipv4_address
+      name         = azurerm_stack_hci_logical_network.test.scale_unit.0.physical_node.1.name
+    }
+
+    storage {
+      configuration_mode = azurerm_stack_hci_logical_network.test.scale_unit.0.storage.0.configuration_mode
+    }
+  }
+}
+`, config)
+}
+
+func (r StackHCILogicalNetworkResource) complete(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%s
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_stack_hci_logical_network" "test" {
+}
+`, template)
+}
+
+func (r StackHCILogicalNetworkResource) template(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+variable "primary_location" {
+  default = %q
+}
+
+variable "random_string" {
+  default = %q
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acctest-hci-vm-${var.random_string}"
+  location = var.primary_location
+}
+
+`, data.Locations.Primary, data.RandomString)
+}
